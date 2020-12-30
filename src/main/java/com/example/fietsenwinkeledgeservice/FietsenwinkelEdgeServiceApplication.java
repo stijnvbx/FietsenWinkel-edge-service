30d38f50e735ded692ad33dda1405a402bb69a07
Iebe <iebe.1@hotmail.com>
2020-12-30 13:58:56 +0100

    init

---
+++ src/main/java/com/example/fietsenwinkeledgeservice/FietsenwinkelEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fietsenwinkeledgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FietsenwinkelEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FietsenwinkelEdgeServiceApplication.class, args);
+    }
+
+}
