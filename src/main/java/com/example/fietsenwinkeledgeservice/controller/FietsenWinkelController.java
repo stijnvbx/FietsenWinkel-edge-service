77b51556a3692b8c76cef3a5d618db89af9cd942
Iebe <iebe.1@hotmail.com>
2020-12-30 14:41:07 +0100

    toevoegen fiets klasse

--- src/main/java/com/example/fietsenwinkeledgeservice/controller/FietsenWinkelController.java
+++ src/main/java/com/example/fietsenwinkeledgeservice/controller/FietsenWinkelController.java
@@ -1,6 +1,7 @@
 package com.example.fietsenwinkeledgeservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,4 +10,18 @@ public class FietsenWinkelController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Value("${fietsenservice.baseurl}")
+    private String fietsenServiceBaseUrl;
+
+    @Value("${onderdeelservice.baseurl}")
+    private String onderdeelServiceBaseUrl;
+
+    @Value("${bestellingservice.baseurl}")
+    private String bestellingServiceBaseUrl;
+
+    @Value("${klantservice.baseurl}")
+    private String klantServiceBaseUrl;
+
+
 }
--- src/main/java/com/example/fietsenwinkeledgeservice/model/Fiets.java
+++ src/main/java/com/example/fietsenwinkeledgeservice/model/Fiets.java
@@ -1,4 +1,69 @@
 package com.example.fietsenwinkeledgeservice.model;
 
 public class Fiets {
+    private String id;
+    private String merk;
+    private String model;
+    private Integer grootte;
+    private Integer prijs;
+    private Integer voorraad;
+
+    public Fiets() {
+    }
+
+    public Fiets(String merk, String model, Integer grootte, Integer prijs, Integer voorraad) {
+        this.merk = merk;
+        this.model = model;
+        this.grootte = grootte;
+        this.prijs = prijs;
+        this.voorraad = voorraad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getGrootte() {
+        return grootte;
+    }
+
+    public void setGrootte(Integer grootte) {
+        this.grootte = grootte;
+    }
+
+    public Integer getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(Integer prijs) {
+        this.prijs = prijs;
+    }
+
+    public Integer getVoorraad() {
+        return voorraad;
+    }
+
+    public void setVoorraad(Integer voorraad) {
+        this.voorraad = voorraad;
+    }
 }
