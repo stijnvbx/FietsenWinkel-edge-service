16886117ec65081263dca4889a6f360bee094428
axei500 <45101479+axei500@users.noreply.github.com>
2021-01-05 15:18:21 +0100

    Got swagger working

--- src/main/java/com/example/fietsenwinkeledgeservice/SpringFoxConfig.java
+++ src/main/java/com/example/fietsenwinkeledgeservice/SpringFoxConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -16,8 +19,22 @@ public class SpringFoxConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .paths(regex("/.*"))
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo()
+    {
+        ApiInfo apiInfo = new ApiInfo(
+                "My Project's REST API",
+                "This is a description of your API.",
+                "version-1",
+                "API TOS",
+                "me@wherever.com",
+                "API License",
+                "API License URL"
+        );
+        return apiInfo;
     }
 
 
